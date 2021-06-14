467e1050b169a503132c528d924e486250eb5e87
Volkan Ozcelik <me@volkan.io>
2021-06-13 22:29:24 -0700

    added some entities

---
+++ pkg/user/user.go
@@ -0,0 +1,33 @@
+/*
+ *  \
+ *  \\,
+ *   \\\,^,.,,.                    “Zero to Hero”
+ *   ,;7~((\))`;;,,               <zerotohero.dev>
+ *   ,(@') ;)`))\;;',    stay up to date, be curious: learn
+ *    )  . ),((  ))\;,
+ *   /;`,,/7),)) )) )\,,
+ *  (& )`   (,((,((;( ))\,
+ */
+
+package user
+
+type Info struct {
+	Email string `json:"email"`
+	Name  string `json:"fullName"`
+}
+
+type Status string
+const Unverified Status = "unverified"
+const Verified Status = "active"
+const Blocked Status = "blocked"
+
+type User struct {
+	Info
+	Password                string `json:"password"`
+	SubscribedToMailingList bool   `json:"subscribedToMailingList"`
+	Status                  Status `json:"status"`
+	EmailVerificationToken  string `json:"accountActivationToken"`
+	PasswordResetToken      string `json:"passwordResetToken"`
+	RecordCreated           int64  `json:"recordCreated"`
+	RecordUpdated           int64  `json:"recordUpdated"`
+}
