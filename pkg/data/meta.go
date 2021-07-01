74af2a29593e97cd5207ddaac9854c89221bed66
Volkan Ozcelik <me@volkan.io>
2021-07-01 07:28:52 -0700

    added metadata

---
+++ pkg/data/meta.go
@@ -0,0 +1,40 @@
+/*
+ *  \
+ *  \\,
+ *   \\\,^,.,,.                     Zero to Hero
+ *   ,;7~((\))`;;,,               <zerotohero.dev>
+ *   ,(@') ;)`))\;;',    stay up to date, be curious: learn
+ *    )  . ),((  ))\;,
+ *   /;`,,/7),)) )) )\,,
+ *  (& )`   (,((,((;( ))\,
+ */
+
+package data
+
+var Keys = struct {
+	Email                  string
+	Status                 string
+	Name                   string
+	EmailVerificationToken string
+	RecordUpdated          string
+	RecordCreated          string
+	Password               string
+}{
+	Email:                  "email",
+	Status:                 "status",
+	Name:                   "Name",
+	EmailVerificationToken: "emailVerificationToken",
+	RecordCreated:          "recordCreated",
+	RecordUpdated:          "recordUpdated",
+	Password:               "password",
+}
+
+var States = struct {
+	Unverified string
+	Verified   string
+	Blocked    string
+}{
+	Unverified: "unverified",
+	Verified:   "verified",
+	Blocked:    "blocked",
+}
