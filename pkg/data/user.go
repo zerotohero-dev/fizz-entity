1e286600b3cdd9d62a988d3f2f2ca22694212de2
Volkan Ozcelik <me@volkan.io>
2021-07-05 00:42:06 -0700

    entity update

--- pkg/data/user.go
+++ pkg/data/user.go
@@ -13,19 +13,14 @@ package data
 
 import "time"
 
-type Info struct {
+type User struct {
+	// Info
 	Email string `json:"email" bson:"email"`
 	Name  string `json:"name" bson:"name"`
-}
-
-type Token struct {
+	// Token
 	EmailVerificationToken string `json:"emailVerificationToken" bson:"emailVerificationToken"`
 	PasswordResetToken     string `json:"passwordResetToken" bson:"passwordResetToken"`
-}
-
-type User struct {
-	Info
-	Token
+	// Rest
 	Password                string    `json:"password" bson:"password"`
 	Status                  string    `json:"status" bson:"status"`
 	SubscribedToMailingList bool      `json:"optIn" bson:"optIn"`
