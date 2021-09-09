12fd9e6c4b65b226a7a665c07fa4648f4d1cfd4e
Volkan Ozcelik <me@volkan.io>
2021-09-08 21:55:54 -0700

    added info response

---
+++ pkg/reqres/user.go
@@ -0,0 +1,24 @@
+/*
+ *  \
+ *  \\,
+ *   \\\,^,.,,.                     Zero to Hero
+ *   ,;7~((\))`;;,,               <zerotohero.dev>
+ *   ,(@') ;)`))\;;',    stay up to date, be curious: learn
+ *    )  . ),((  ))\;,
+ *   /;`,,/7),)) )) )\,,
+ *  (& )`   (,((,((;( ))\,
+ */
+
+package reqres
+
+type UserInfoRequest struct {
+	AuthToken string `json:"token"`
+	Err       string `json:"err,omitempty"`
+}
+
+type UserInfoResponse struct {
+	Email         string   `json:"email"`
+	Subscriptions []string `json:"courses"`
+	Name          string   `json:"name"`
+	Err           string   `json:"err,omitempty"`
+}
