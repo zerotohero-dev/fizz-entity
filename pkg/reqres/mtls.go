e7c6950c9d381c402822b90d4b1c0d39e534e7ba
Volkan Ozcelik <me@volkan.io>
2021-11-08 17:21:14 -0800

    added mtls api request

---
+++ pkg/reqres/mtls.go
@@ -0,0 +1,17 @@
+/*
+ *  \
+ *  \\,
+ *   \\\,^,.,,.                     Zero to Hero
+ *   ,;7~((\))`;;,,               <zerotohero.dev>
+ *   ,(@') ;)`))\;;',    stay up to date, be curious: learn
+ *    )  . ),((  ))\;,
+ *   /;`,,/7),)) )) )\,,
+ *  (& )`   (,((,((;( ))\,
+ */
+
+package reqres
+
+type MtlsApiRequest struct {
+	Endpoint string `json:"endpoint"`
+	Method   string `json:"method"`
+}
