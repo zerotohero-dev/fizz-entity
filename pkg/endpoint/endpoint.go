4b0347d4a934126e247bc392b5c40feeb57d55ca
Volkan Ozcelik <me@volkan.io>
2021-11-12 15:45:31 -0800

    added method

--- pkg/endpoint/endpoint.go
+++ pkg/endpoint/endpoint.go
@@ -1,3 +1,14 @@
+/*
+ *  \
+ *  \\,
+ *   \\\,^,.,,.                     Zero to Hero
+ *   ,;7~((\))`;;,,               <zerotohero.dev>
+ *   ,(@') ;)`))\;;',    stay up to date, be curious: learn
+ *    )  . ),((  ))\;,
+ *   /;`,,/7),)) )) )\,,
+ *  (& )`   (,((,((;( ))\,
+ */
+
 package endpoint
 
 var Crypto = struct {
---
+++ pkg/method/methods.go
@@ -0,0 +1,19 @@
+/*
+ *  \
+ *  \\,
+ *   \\\,^,.,,.                     Zero to Hero
+ *   ,;7~((\))`;;,,               <zerotohero.dev>
+ *   ,(@') ;)`))\;;',    stay up to date, be curious: learn
+ *    )  . ),((  ))\;,
+ *   /;`,,/7),)) )) )\,,
+ *  (& )`   (,((,((;( ))\,
+ */
+
+package method
+
+type Method string
+
+var Get Method = "GET"
+var Post Method = "POST"
+var Put Method = "PUT"
+var Patch Method = "PATCH"
